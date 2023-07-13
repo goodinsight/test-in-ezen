5ebeee26581b5c768ef83b74fc20e786f0e2cbe9
Lee Keunseop <dlrmstjq1@gmail.com>
2023-07-13 23:24:43 +0900

    spring 환경 설정 및 dataSourceTest / mybatisTest

---
+++ springworkspaces/testspring/src/test/java/kr/co/dong/MybatisTest.java
@@ -0,0 +1,33 @@
+package kr.co.dong;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class MybatisTest {
+	@Inject
+	private SqlSessionFactory sqlFactory;
+	
+	@Test
+	public void sqlFactory() {
+		System.out.println(sqlFactory);
+	}
+	
+	@Test
+	public void sqlSession() throws Exception {
+		try {
+			SqlSession session=sqlFactory.openSession();
+			System.out.println(session);
+		} catch(Exception e) {
+			e.fillInStackTrace();
+		}
+	}
+}
