356b872e86b0ebf3ba44762889689186975df796
A-19\168 <goodinsight@ajou.ac.kr>
2023-09-11 18:00:59 +0900

    security 회원가입 join 초기 화면까지 p734 실습완료

--- IntelliJworkspaces/b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ IntelliJworkspaces/b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -75,6 +75,7 @@ public class BoardController {
 
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping({"/read","/modify"})
     public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
 
@@ -86,7 +87,7 @@ public class BoardController {
         model.addAttribute("dto", boardDTO);
 
     }
-
+    @PreAuthorize("principal.username == #boardDTO.writer")
     @PostMapping("/modify")
     public String modify(PageRequestDTO pageRequestDTO,
                          @Valid BoardDTO boardDTO,
@@ -117,6 +118,7 @@ public class BoardController {
 
     }
 
+    @PreAuthorize("principal.username == #boardDTO.writer")
     @PostMapping("/remove")
     public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
--- IntelliJworkspaces/b01/src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
+++ IntelliJworkspaces/b01/src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package org.zerock.b01.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,28 +10,57 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.dto.MemberSecurityDTO;
+import org.zerock.b01.repository.MemberRepository;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor // 추가
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
 
     private PasswordEncoder passwordEncoder;
 
-    public CustomUserDetailsService() {
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
+//    public CustomUserDetailsService() {
+//        this.passwordEncoder = new BCryptPasswordEncoder();
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.info("loadUserByUsername: " + username);
 
-        UserDetails userDetals = User.builder()
-                .username("user1")
-                .password(passwordEncoder.encode("1111"))
-                .authorities("ROLE_USER")
-                .build();
+        Optional<Member> result = memberRepository.getWithRoles(username);
 
-        return userDetals;
+        if(result.isEmpty()) {  //해당 아이디를 가진 사용자가 없다면
+            throw new UsernameNotFoundException("username not found.....");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+                member.getMid(),
+                member.getMpw(),
+                member.getEmail(),
+                member.isDel(),
+                false,
+                member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_"+memberRole.name())).collect(Collectors.toList())
+        );
+
+        log.info("memberSecurityDTO");
+        log.info(memberSecurityDTO);
+
+        return memberSecurityDTO;
+//        UserDetails userDetals = User.builder()
+//                .username("user11")
+//                .password(passwordEncoder.encode("1111"))
+//                .authorities("ROLE_USER")
+//                .build();
+//        return userDetals;
     }
 }
