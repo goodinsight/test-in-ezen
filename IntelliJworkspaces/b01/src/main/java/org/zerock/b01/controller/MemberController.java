3a11d917b7f49160ddbb36c6a6a39afc087e9ba5
A-19\168 <goodinsight@ajou.ac.kr>
2023-09-14 08:56:22 +0900

    멤버 로그인 까지 완료 // 카카오 로그인 연습필요

--- IntelliJworkspaces/b01/src/main/java/org/zerock/b01/controller/MemberController.java
+++ IntelliJworkspaces/b01/src/main/java/org/zerock/b01/controller/MemberController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.MemberJoinDTO;
+import org.zerock.b01.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
@@ -14,8 +16,11 @@ import org.zerock.b01.dto.MemberJoinDTO;
 @RequiredArgsConstructor
 public class MemberController {
 
+    //의존성 주입
+    private final MemberService memberService;
+
     @GetMapping("/login")
-    public void loginGET(String error, String logout) {
+    public void loginGET(String errorCode, String logout) {
         log.info("login get.......");
         log.info("logout: " + logout);
 
@@ -33,12 +38,21 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinPOST(MemberJoinDTO memberJoinDTO) {
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes) {
 
         log.info("join post.....");
         log.info(memberJoinDTO);
 
-        return "redirect:/board/list";
+        try{
+            memberService.join(memberJoinDTO);
+        } catch (MemberService.MidExistException e) {
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        return "redirect:/member/login"; //회원가입후 로그인
 
     }
 
